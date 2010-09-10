7d36ce833679c756858b951742dca7c5c6e8bdb2
Georg Ledermann <mail@georg-ledermann.de>
2010-09-10 08:00:23 +0200

    Every country has its own file for area_codes

---
+++ lib/dialy/data/countries.rb
@@ -0,0 +1,26 @@
+module Dialy
+  COUNTRY_CODES = [
+    # TODO: Add more from http://en.wikipedia.org/wiki/List_of_country_calling_codes
+    
+    # Zone 1
+    1,
+    
+    # Zone 2
+
+    # Zone 3
+
+    # Zone 4
+    41,
+    43,
+    49,
+
+    # Zone 5
+
+    # Zone 6
+
+    # Zone 7
+
+    # Zone 8
+    800
+  ].freeze
+end
